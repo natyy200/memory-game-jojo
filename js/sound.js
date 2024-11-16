78559d13df4829e151841b56790e928c35efc3d9
Natyy__ <131829677+natyy200@users.noreply.github.com>
2024-11-16 16:38:03 -0300

    Add files via upload

--- js/sound.js
+++ js/sound.js
@@ -1,6 +1,6 @@
 const principal = document.querySelector(".principal");
 
-const music = new Audio("../sounds/videoplayback.m4a");
-music.volume = 1;
+const music = new Audio("../sounds/audio.mp3");
+music.volume = 0.3;
 
 principal.addEventListener('mouseover', ()=>music.play());
